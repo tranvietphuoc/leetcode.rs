4121c1d5e17a670a6a8eac6a3c261c8fc65de129
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2023-01-30 16:29:53 +0700

    edit two sum

--- src/three_sum_closest.rs
+++ src/three_sum_closest.rs
@@ -1,6 +1,9 @@
 pub struct Solution;
 
 impl Solution {
+    use std::cmp;
+    use std::collections::HashMap;
+
     pub fn three_sum_closest(nums: Vec<i32>, target: i32) -> i32 {
         use std::cmp;
         use std::collections::HashMap;
@@ -8,6 +11,27 @@ impl Solution {
         let mut result = 0;
         let mut counter = HashMap::new();
 
-        result
+
+
+    }
+
+    fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>{
+        use std::cmp;
+        use std::collections::HashMap;
+
+        let mut result = Vec::new();
+        let mut counter = HashMap::new();
+
+        let mut tar = target;
+
+        for (i, val) in nums.into_iter().enumerate(){
+            let remainder = tar - val;
+            if !counter.contains_key(&remainder){
+                counter.insert(val, i as i32);
+            } else {
+                result.push(counter[&remainder]);
+                result.push(i as i32);
+            }
+        }
     }
 }
--- src/two_sum.rs
+++ src/two_sum.rs
@@ -6,21 +6,16 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         use std::collections::HashMap;
 
-        let mut result = Vec::new();
-
         let mut tar = target;
         let mut ht = HashMap::new();
-
         for (i, &e) in nums.iter().enumerate() {
             let remainder = tar - e;
             if !ht.contains_key(&remainder) {
                 ht.insert(e, i as i32);
             } else {
-                result.push(ht[&remainder]);
-                result.push(i as i32);
+                return vec![ht[&remainder], i as i32];
             }
         }
-
-        return result;
+        vec![]
     }
 }
