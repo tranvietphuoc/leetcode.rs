ec7552e23fa4becfde2af8bd763a71041c2be204
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2023-02-06 19:51:45 +0700

    other add digits

--- src/add_digits.rs
+++ src/add_digits.rs
@@ -33,13 +33,13 @@ impl Solution {
         result
     }
 
-    /* pub fn add_digits(num: i32) -> i32 {
+    /* pub fn add_digits(mut num: i32) -> i32 {
         match num {
             0 => return 0,
             _ => {
                 num = num % 9;
                 if num == 0 {
-                    return 0;
+                    return 9;
                 } else {
                     return num;
                 }
