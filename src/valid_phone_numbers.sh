cc5d67ac5838de0e935c489741cdb2d90b6ac6d5
tran viet phuoc <phuoc@phuocs-MacBook-Pro.local>
2023-02-09 04:42:17 +0700

    valid phone numbers in bash

---
+++ src/valid_phone_numbers.sh
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/valid-phone-numbers/
+
+#!/bin/bash
+
+# Read from the file file.txt and output all valid phone numbers to stdout.
+grep -P '(^\(\d{3}\) \d{3}\-\d{4}$)|(^\d{3}\-\d{3}\-\d{4}$)' file.txt
